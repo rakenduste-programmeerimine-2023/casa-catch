837fb2a11544a8bf3d6ee3f1f84f20cddb98a018
Roland <rsilt@tlu.ee>
2023-11-08 18:39:04 +0200

    feat(UI) - added footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./components.css"
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <div className="footer-content">
+        Powered by Roland
+        <div className="footer-links">
+          <a href="https://github.com/rakenduste-programmeerimine-2023/casa-catch">GitHub</a>
+          <a href="https://www.kv.ee/">KV.ee</a>
+          <a href="https://www.city24.ee/">City24.ee</a>
+          <a href="https://rendin.ee/">Rendin.ee</a>
+          <a href="http://kv24.ee/">KV24.ee</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+
