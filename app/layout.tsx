12115401938c50ff5a497091963503bde5341e97
Roland <rsilt@tlu.ee>
2023-11-08 17:29:59 +0200

    feat(UI) - formatted UI code

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'casa-catch',
+  description: 'Casa-Catch projekt on mõeldud kinnisvara kiireks ja lihtsaks leidmiseks.',
 }
 
 export default function RootLayout({
