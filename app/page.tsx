4ffc18a8394be376d989803bd1a5a69183960dd3
Roland <rsilt@tlu.ee>
2023-11-08 18:39:04 +0200

    feat(UI) - added footer component

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,7 @@ export default async function Index() {
       </div>
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          {/*TODO create separate component for the footer here*/}
-          Powered by
-            Roland
-        </p>
+       <Footer/>
       </footer>
     </div>
   )
