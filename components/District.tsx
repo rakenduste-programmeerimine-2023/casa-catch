1f3208c2ef383f72f3a088f0360bf307743f1a4e
Jan Henrik Levertand <janlever@Jans-MacBook-Pro.local>
2023-11-28 17:43:13 +0200

    WIP: Use useState for Rooms and Pricerange components

--- components/District.tsx
+++ components/District.tsx
@@ -3,16 +3,27 @@ import Chip from '@mui/material/Chip';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
+import { useState } from 'react';
+
+interface District {
+  title: string;
+}
 
 export default function Tags() {
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
+  const handleTagDelete = (tagToDelete: string) => {
+    setSelectedTags((prevTags) => prevTags.filter((tag) => tag !== tagToDelete));
+  };
+
   return (
     <Stack spacing={3} sx={{ width: 500 }}>
       <Autocomplete
         multiple
         id="tags-standard"
         options={districts}
-        getOptionLabel={(option) => option.title}
-        defaultValue={[districts[5]]}
+        value={selectedTags}
+        onChange={(_, newValue) => setSelectedTags(newValue)}
         renderInput={(params) => (
           <TextField
             {...params}
@@ -22,23 +33,23 @@ export default function Tags() {
           />
         )}
       />
-     
-    
+      {selectedTags.map((tag) => (
+        <Chip key={tag} label={tag} onDelete={() => handleTagDelete(tag)} />
+      ))}
     </Stack>
   );
 }
 
-const districts = [
-  
-  { title: 'Haabersti'},
-  { title: 'Kadriorg'},
-  { title: 'Kesklinn'},
-  { title: "Kristiine"},
-  { title: 'Lasnamäe'},
-  { title: 'Mustamäe'},
-  { title: 'Nõmme'},
+const districts: District[] = [
+  { title: 'Haabersti' },
+  { title: 'Kadriorg' },
+  { title: 'Kesklinn' },
+  { title: 'Kristiine' },
+  { title: 'Lasnamäe' },
+  { title: 'Mustamäe' },
+  { title: 'Nõmme' },
   { title: 'Pirita' },
-  { title: 'Põhja-Tallinn'},
-  { title: 'Vanalinn'},
-  { title: 'Kalamaja'}
+  { title: 'Põhja-Tallinn' },
+  { title: 'Vanalinn' },
+  { title: 'Kalamaja' },
 ];
