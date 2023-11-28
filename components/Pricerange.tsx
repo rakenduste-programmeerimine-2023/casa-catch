9b683c7563d92bb6a6dae0e49abcc8981ac91c52
Jan Henrik Levertand <janlever@Jans-MacBook-Pro.local>
2023-11-28 17:43:13 +0200

    WIP: Use useState for Rooms and Pricerange components

--- components/Pricerange.tsx
+++ components/Pricerange.tsx
@@ -1,8 +1,25 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { useState } from 'react';
 
-export default function FormPropsTextFields() {
+export default function Pricerange() {
+  const [minPrice, setMinPrice] = useState<number | null>(null)
+  const [maxPrice, setMaxPrice] = useState<number | null>(null)
+
+  console.log("minPrice value: ", minPrice)
+  console.log("maxPrice value: ", maxPrice)
+
+  const handleMinPriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(event.target.value, 10)
+    setMinPrice(isNaN(value) ? null : value)
+  }
+
+  const handleMaxPriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(event.target.value, 10)
+    setMaxPrice(isNaN(value) ? null : value)
+  }
+
   return (
     <Box
       component="form"
@@ -14,17 +31,21 @@ export default function FormPropsTextFields() {
     >
       <div>
         <TextField
-          id="rooms"
+          id="minPrice"
           label="Hind alates"
           type="number"
+          value={minPrice !== null ? minPrice : ''}
+          onChange={handleMinPriceChange}
           InputLabelProps={{
             shrink: true,
           }}
         />
         <TextField
-          id="rooms"
+          id="maxPrice"
           label="Hind kuni"
           type="number"
+          value={maxPrice !== null ? maxPrice : ''}
+          onChange={handleMaxPriceChange}
           InputLabelProps={{
             shrink: true,
           }}
--- components/Rooms.tsx
+++ components/Rooms.tsx
@@ -1,8 +1,26 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { useState } from 'react';
 
-export default function FormPropsTextFields() {
+export default function Rooms() {
+
+  const [minRooms, setMinRooms] = React.useState<number | null>(null)
+  const [maxRooms, setMaxRooms] = React.useState<number | null>(null)
+
+  console.log("minRooms value: ",minRooms)
+  console.log("maxRooms value: ",maxRooms)
+
+  const handleMinRoomsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(event.target.value, 10)
+    setMinRooms(isNaN(value) ? null : value)
+  }
+
+  const handleMaxRoomsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(event.target.value, 10)
+    setMaxRooms(isNaN(value) ? null : value)
+  }
+
   return (
     <Box
       component="form"
@@ -14,17 +32,21 @@ export default function FormPropsTextFields() {
     >
       <div>
         <TextField
-          id="rooms"
+          id="minRooms"
           label="Tubade arv alates"
           type="number"
+          value={minRooms !== null ? minRooms : ''}
+          onChange={handleMinRoomsChange}
           InputLabelProps={{
             shrink: true,
           }}
         />
         <TextField
-          id="rooms"
+          id="maxRooms"
           label="Tubade arv kuni"
           type="number"
+          value={maxRooms !== null ? maxRooms : ''}
+          onChange={handleMaxRoomsChange}
           InputLabelProps={{
             shrink: true,
           }}
