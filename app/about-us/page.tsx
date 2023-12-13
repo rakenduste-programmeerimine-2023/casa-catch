990eaba87e98ce42cd2c943c68e1b9feb20c6bdc
makuska <sander0204@gmail.com>
2023-12-13 11:41:08 +0200

    refactor (frontend): Fix info about the team

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Container, Typography, Paper, Grid, AppBar, Toolbar } from '@mui/material';
-//import teamImage from 'file:///Users/rolandsilt/Desktop/CasaCatch/Meie.jpg';
 
 const AboutUsPage: React.FC = () => {
-  const teamImage = '//Users/rolandsilt/Desktop/CasaCatch/Meie.jpg'
   return (
     <div>
       <AppBar position="static">
@@ -18,13 +16,13 @@ const AboutUsPage: React.FC = () => {
             Our Story
           </Typography>
           <Typography variant="body1">
-          We came up with CasaCatch because we wanted to make life easier for folks looking for their dream homes. 
-          Realizing how all over the place the real estate game is, our team of three computer science students from Tallinn University decided to shake things up a bit.
-          Our goal? To take the hassle out of finding the perfect property.
-          We saw people juggling through different platforms, each with its own quirks, and it looked like a real headache.
-          So, as a bunch of second-year students we thought, "Hey, why not create something that brings all these real estate listings under one roof?"
-          That's how CasaCatch came to be – our attempt to lighten up the property search scene.
-          No more bouncing around from one site to another. With CasaCatch, you can kick back, set your preferences, and let the house hunt come to you. 
+            We came up with CasaCatch because we wanted to make life easier for folks looking for their dream homes.
+            Realizing how all over the place the real estate game is, our team of three computer science students from Tallinn University decided to shake things up a bit.
+            Our goal? To take the hassle out of finding the perfect property.
+            We saw people juggling through different platforms, each with its own quirks, and it looked like a real headache.
+            So, as a bunch of second-year students we thought, "Hey, why not create something that brings all these real estate listings under one roof?"
+            That's how CasaCatch came to be – our attempt to lighten up the property search scene.
+            No more bouncing around from one site to another. With CasaCatch, you can kick back, set your preferences, and let the house hunt come to you.
           </Typography>
         </Paper>
 
@@ -32,12 +30,9 @@ const AboutUsPage: React.FC = () => {
           <Grid item xs={12} md={6}>
             <Paper style={{ padding: 16 }}>
               <Typography variant="h5" gutterBottom>
-                Our Mission
-              </Typography>
-              <Typography variant="body1">
-              CasaCatch is designed for real estate seekers who wish to conveniently monitor all real estate platforms through a single central webpage. 
-              The application allows users to create a personal account where they can specify their preferred real estate search criteria, 
-              such as price range, location, type, and other desired features, without complications.
+                CasaCatch is designed for real estate seekers who wish to conveniently monitor all real estate platforms through a single central webpage.
+                The application allows users to create a personal account where they can specify their preferred real estate search criteria,
+                such as price range, location, type, and other desired features, without complications.
               </Typography>
             </Paper>
           </Grid>
@@ -47,12 +42,11 @@ const AboutUsPage: React.FC = () => {
               <Typography variant="h5" gutterBottom>
                 Meet the Team
               </Typography>
-              <img src={teamImage}/>
               <Typography variant="body1">
-              Our team comprises three second-year Computer Science students from Tallinn University (TLÜ). 
-              The leadership role is undertaken by Sander Nõlvak, who adeptly managed the project and primarily focused on backend development. 
-              In addition, Roland Silt and Jan Henrik Levertand serve as frontend developers, 
-              demonstrating versatility by engaging in supplementary tasks such as creating Python snippets and occasionally jumping into backend responsibilities.
+                Our team comprises three second-year Computer Science students from Tallinn University (TLÜ).
+                The leadership role is undertaken by Sander Nõlvak, who adeptly managed the project and primarily focused on backend development.
+                In addition, Roland Silt and Jan Henrik Levertand serve as frontend developers,
+                demonstrating versatility by engaging in supplementary tasks such as creating Python snippets and occasionally jumping into backend responsibilities.
               </Typography>
             </Paper>
           </Grid>
